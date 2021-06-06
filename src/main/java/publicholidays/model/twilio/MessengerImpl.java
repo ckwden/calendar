24db1173289a2f2c6d0d18a6a788d797117b872d
cden4725 <cden4725.uni.sydney.edu.au>
2021-06-06 23:34:49 +1000

    Removed print statement

--- src/main/java/publicholidays/model/twilio/MessengerImpl.java
+++ src/main/java/publicholidays/model/twilio/MessengerImpl.java
@@ -67,7 +67,6 @@ public class MessengerImpl implements Messenger {
                 response.append(line);
             }
             reader.close();
-            System.out.println(response);
             http.disconnect();
         } catch (MalformedURLException e) {
             System.out.println("Invalid URL");
