01b2868159f3853b6e2ac0358c1bda883622f640
cden4725 <cden4725.uni.sydney.edu.au>
2021-06-06 20:50:08 +1000

    No longer using Twilio library

--- README.md
+++ README.md
@@ -31,3 +31,4 @@ I have implemented the required features for distinction.
 ## Citations
 https://stackoverflow.com/questions/50552075/datepicker-only-mark-certain-days-with-the-color-red  
 https://stackoverflow.com/questions/34681975/javafx-extract-calendar-popup-from-datepicker-only-show-popup/34684268  
+https://www.baeldung.com/java-http-url-connection
--- src/main/java/publicholidays/model/twilio/MessengerImpl.java
+++ src/main/java/publicholidays/model/twilio/MessengerImpl.java
@@ -1,8 +1,14 @@
 package publicholidays.model.twilio;
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * Implements the Messenger interface
@@ -13,17 +19,60 @@ public class MessengerImpl implements Messenger {
     private final String token;
     private final String numberTo;
     private final String numberFrom;
+    private final String url;
 
     public MessengerImpl(String sid, String token, String numberTo, String numberFrom) {
         this.sid = sid;
         this.token = token;
         this.numberTo = numberTo;
         this.numberFrom = numberFrom;
+        this.url = "https://api.twilio.com/2010-04-01/";
     }
 
     @Override
     public void sendReport(String report) {
-        Twilio.init(sid, token);
-        Message.creator(new PhoneNumber(numberTo), new PhoneNumber(numberFrom), report).create();
+        String body = report.replaceAll(" ", "%20");
+        BufferedReader reader;
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(this.url + "Accounts/" + this.sid + "/Messages.json?to=" + this.numberTo +
+                    "&from=" + this.numberFrom + "&body=" + body);
+            System.out.println(url.toString());
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            http.setRequestProperty("Accept", "application/json");
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            // https://www.baeldung.com/java-http-url-connection
+            String auth = this.sid + ":" + this.token;
+            byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
+            String authHeader = "Basic " + new String(encodedAuth);
+            http.setRequestProperty("Authorization", authHeader);
+
+            String toSend = "To=" + this.numberTo.replace("+", "%2B")
+                    + "&From=" + this.numberFrom.replace("+", "%2B")
+                    + "&Body=" + body;
+            byte[] encodedToSend = toSend.getBytes(StandardCharsets.UTF_8);
+            OutputStream os = http.getOutputStream();
+            os.write(encodedToSend);
+
+            String line;
+            int status = http.getResponseCode();
+            if (status > 299) {
+                reader = new BufferedReader(new InputStreamReader(http.getErrorStream()));
+            } else {
+                reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            }
+            while((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            System.out.println(response);
+            http.disconnect();
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid URL");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
